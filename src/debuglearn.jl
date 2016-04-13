1d847ebda7badb5cf24fed63d60c2352330350a0
SVAKSHA <svaksha@gmail.com>
2016-04-13 15:46:41 +0200

    clean up

--- src/debuglearn.jl
+++ src/debuglearn.jl
@@ -1,20 +1,13 @@
-#push!(LOAD_PATH, pwd())
+push!(LOAD_PATH, pwd())
 
 #using TimerOutputs
 using DebuggingUtilities
 
-
-module debuglearn
-
 function foo()
     x = 5
-    println(time_showline, x)
     @showln x
-    println(x)
     x = 7
     @showln x
     nothing
-    println(x)
-end
+end  # function foo()
 
-end
